9a238d6d6da08a84d775ef9220a6be66f890dc3f
jreeserunyan <jreeserunyan@gmail.com>
2015-12-27 23:39:59 -0600

    Create posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root "application#hello"
+  resources :posts
+  resources :users
+
+  # get 'user/index'
+  # get 'user/edit'
+  # get 'user/new'
+  # get 'user/show'
+
+  root "users#index"
 end
